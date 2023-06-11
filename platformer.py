55db4def9fd7b452faff7ba2f49cafdf071d42cc
nerd-man <106038467+nerd-man@users.noreply.github.com>
2023-06-11 15:54:36 -0600

    can now build in platformer
    fixed crash when deleting non-existing tiles

--- platformer.py
+++ platformer.py
@@ -136,13 +136,14 @@ while True:
     pos = pygame.mouse.get_pos()
     x = (pos[0]+(scroll[0]*2)) // (TILESIZE*2)
     y = (pos[1]+(scroll[1]*2)) // (TILESIZE*2)
-
-    if pygame.mouse.get_pressed()[0]:
-        if 0 <= x < 64 and 0 <= y < 64:
-            if map[y][x] != '1':
-                map[y][x] = '1'
-    elif pygame.mouse.get_pressed()[2]:
-        map[y][x] = '0'
+    if 0 <= x < 64 and 0 <= y < 64:
+        if pygame.mouse.get_pressed()[0]:
+            if map[y][x] not in no_collide:
+                if map[y][x] != '1':
+                    map[y][x] = '1'
+        elif pygame.mouse.get_pressed()[2]:
+            if map[y][x] not in no_collide:
+                map[y][x] = '0'
 
     surf = pygame.transform.scale(display, WINDOWSIZE)
     screen.blit(surf, (0,0))
